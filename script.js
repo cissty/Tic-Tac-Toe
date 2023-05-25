a9c39b77295a1b757fdabef409fefd154497e8fb
cissty <128071188+cissty@users.noreply.github.com>
2023-05-25 17:22:51 +0300

    updated ai section. check styling and fix player section

--- script.js
+++ script.js
@@ -1,128 +1,184 @@
-
-//store the gameboard as an array inside of a Gameboard object
-//players are also going to be stored in objects
-//control the flow of the game itself.
-
-const firstSection = document.querySelector('.menu')
-const secondSection = document.querySelector('.game-board')
+const firstSection = document.querySelector('.menu');
+const secondSection = document.querySelector('.game-board');
 const container = document.querySelector('.main-container');
-const playerTexts = document.querySelector('.players-texts')
-const yourTurn = document.querySelector('.player-turn')
-const aiTexts = document.querySelector('.ai-texts')
-const divNodeList = document.querySelectorAll('.div')
-const aiSelection = document.getElementById('ai-selection')
-const playerSelection = document.getElementById('player-selection')
+const playerTexts = document.querySelector('.players-texts');
+const yourTurn = document.querySelector('.player-turn');
+const aiTexts = document.querySelector('.ai-texts');
+const divNodeList = document.querySelectorAll('.div');
 const modal = document.getElementById("myModal");
 const span = document.getElementsByClassName("close")[0];
-const modalText = document.getElementById('modal-text')
-const arrow = document.getElementById('back-arrow')
-
-aiSelection.addEventListener('click', () =>{
-    firstSection.style.display = 'none' 
-    secondSection.style.display = 'flex'
-    aiTexts.style.display = 'block'
-    arrow.style.display = 'block'
-})
-
-playerSelection.addEventListener('click', () =>{
-    firstSection.style.display = 'none' 
-    secondSection.style.display = 'flex'
-    playerTexts.style.display = 'grid'
-    arrow.style.display = 'block'
-
-})
-
-
-const GameBoard = {
-    gameBoard: [divNodeList[0], divNodeList[1], divNodeList[2],
-                divNodeList[3], divNodeList[4], divNodeList[5],
-                divNodeList[6], divNodeList[7], divNodeList[8],
-                ],
-}
+const modalText = document.getElementById('modal-text');
+const arrow = document.getElementById('back-arrow');
+const aiSelection = document.getElementById('ai-selection');
+const playerSelection = document.getElementById('player-selection');
 
 let currentPlayer = 'X';
-let newTurn = "Player 1's turn."
+let newTurn = "Player 1's turn.";
+let gameMode = 'player'; // player vs. player
 
+const againstPlayer = (() => {
+  const checkWin = (player) => {
+    const winningCombinations = [
+      // Rows
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // Columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // Diagonals
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
 
-    const againstPlayer =  (() => {
-    
-    const detectMarker = divNodeList.forEach(function(el){
-        el.addEventListener('click', () => {
-            el.textContent = currentPlayer;
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-
-            newTurn = newTurn === "Player 1's turn." ? "Player 2's turn." : "Player 1's turn."
-            yourTurn.textContent = newTurn;
-            const winningCombinations = [
-              // Rows
-              [0, 1, 2],
-              [3, 4, 5],
-              [6, 7, 8],
-              // Columns
-              [0, 3, 6],
-              [1, 4, 7],
-              [2, 5, 8],
-              // Diagonals
-              [0, 4, 8],
-              [2, 4, 6]
-            ];
-            
-            function checkWin(player) {
-              for (const combination of winningCombinations) {
-                if (
-                  divNodeList[combination[0]].textContent === player &&
-                  divNodeList[combination[1]].textContent === player &&
-                  divNodeList[combination[2]].textContent === player
-                ) {
-                  return true; // Win condition met
-                }
-              }
-              return false; // No win condition met
-            }
-            
-            if (checkWin('X')) {
-              modalText.textContent = `Player 1 has won the game. `
-              modal.style.display = "block";
-             
-            } else if (checkWin('O')) {
-              modalText.textContent = `Player 2 has won the game. `
-              modal.style.display = "block";
-            }
-             
-            let allBoxesNotEmpty = true;
-            for (let i = 0; i < divNodeList.length; i++) {
-              if (divNodeList[i].textContent.trim() === '') {
-                allBoxesNotEmpty = false;
-                break;
-              }
-            }
-
-            if (allBoxesNotEmpty) {
-              modalText.textContent = `It's a tie `
-              modal.style.display = "block";
-            }
-          })
-
-    })
-    
-    return {
-        detectMarker
+    for (const combination of winningCombinations) {
+      if (
+        divNodeList[combination[0]].textContent === player &&
+        divNodeList[combination[1]].textContent === player &&
+        divNodeList[combination[2]].textContent === player
+      ) {
+        return true; // Win condition met
+      }
     }
+    return false; // No win condition met
+  };
+
+  const isGameOver = () => {
+    if (checkWin('X')) {
+      modalText.textContent = "Player 1 has won the game.";
+      modal.style.display = "block";
+      return true;
+    } else if (checkWin('O')) {
+      modalText.textContent = "Player 2 has won the game.";
+      modal.style.display = "block";
+      return true;
+    }
+
+    const emptyBoxes = getEmptyBoxes();
+    if (emptyBoxes.length === 0) {
+      modalText.textContent = "It's a tie.";
+      modal.style.display = "block";
+      return true;
+    }
+
+    return false;
+  };
+
+  const playerTurn = (el) => {
+    if (!el.textContent && !isGameOver()) {
+      el.textContent = currentPlayer;
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+
+    newTurn = newTurn === "Player 1's turn." ? "Player 2's turn." : "Player 1's turn.";
+    yourTurn.textContent = newTurn;
+
+    el.classList.add('clicked');
+    setTimeout(() => {
+      el.classList.remove('clicked');
+    }, 500);
+  };
+
+  const aiTurn = () => {
+    if (!isGameOver() && currentPlayer === 'O') {
+      const bestMoveIndex = minimax(0, 'O');
+      divNodeList[bestMoveIndex].textContent = currentPlayer;
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+  };
+
+  const getEmptyBoxes = () => {
+    return Array.from(divNodeList).reduce((emptyBoxes, box, index) => {
+      if (!box.textContent) {
+        emptyBoxes.push(index);
+      }
+      return emptyBoxes;
+    }, []);
+  };
+
+  const minimax = (depth, player) => {
+    if (checkWin('X')) {
+      return depth - 10; // Player 1 wins
+    } else if (checkWin('O')) {
+      return 10 - depth; // Player 2 (computer) wins
+    } else if (getEmptyBoxes().length === 0) {
+      return 0; // It's a tie
+    }
+
+    const scores = [];
+    const moves = [];
+
+    getEmptyBoxes().forEach((index) => {
+      divNodeList[index].textContent = player;
+
+      if (player === 'X') {
+        scores.push(minimax(depth + 1, 'O'));
+      } else {
+        scores.push(minimax(depth + 1, 'X'));
+      }
+
+      moves.push(index);
+      divNodeList[index].textContent = '';
+    });
+
+    const bestMoveIndex = player === 'X' ? moves[scores.indexOf(Math.max(...scores))] : moves[scores.indexOf(Math.min(...scores))];
+    return bestMoveIndex;
+  };
+
+  divNodeList.forEach((el) => {
+    el.addEventListener('click', () => {
+      if (gameMode === 'player') {
+        playerTurn(el);
+      } else if (gameMode === 'ai') {
+        if (currentPlayer === 'X') {
+          playerTurn(el);
+        }
+      }
+
+      if (gameMode === 'ai') {
+        aiTurn();
+      }
+    });
+  });
+
+  return {
+    setGameMode: (mode) => {
+      gameMode = mode;
+    }
+  };
 })();
+
+aiSelection.addEventListener('click', () => {
+  firstSection.style.display = 'none';
+  secondSection.style.display = 'flex';
+  aiTexts.style.display = 'block';
+  arrow.style.display = 'block';
+
+  againstPlayer.setGameMode('ai');
+});
+
+playerSelection.addEventListener('click', () => {
+  firstSection.style.display = 'none';
+  secondSection.style.display = 'flex';
+  playerTexts.style.display = 'grid';
+  arrow.style.display = 'block';
+
+  againstPlayer.setGameMode('player');
+});
 
 span.onclick = function() {
   modal.style.display = "none";
-  location.reload()
-}
+  location.reload();
+};
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    location.reload()
+    location.reload();
   }
-}
+};
 
 arrow.onclick = function(){
-  location.reload()
-}
-
+  location.reload();
+};
